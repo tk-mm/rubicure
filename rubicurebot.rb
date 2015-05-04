a5b59dcdec3bf8f68663ae2f786171b73b879bf2
uzuyh <miller_sphere@yahoo.co.jp>
2015-05-05 02:35:51 +0900

    正規表現を変更2

--- rubicurebot.rb
+++ rubicurebot.rb
@@ -26,7 +26,7 @@ stream_client.user do |status|
   next unless status.is_a? Twitter::Tweet
   next if status.text.start_with? "RT"
 
-  if status.text =~ /^ぷりきゅあ/
+  if status.text =~ /^ぷりきゅあ$/
     option = {"in_reply_to_status_id" => status.id.to_s }
     tweet = []
     tweet = Cure.peace.transform!
@@ -36,7 +36,7 @@ stream_client.user do |status|
       client.update "@#{status.user.screen_name} #{v}", option
     end
   end
-  if status.text =~ /^name/
+  if status.text =~ /^name$/
     option = {"in_reply_to_status_id" => status.id.to_s }
     tweet = "@#{status.user.screen_name} #{Cure.peace.name}"
     client.update tweet, option
